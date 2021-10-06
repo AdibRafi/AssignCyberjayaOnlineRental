ed776e497e35306d90ecdd6fa568266823b2837a
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-10-06 12:40:57 +0800

    Update backend stuff Can use ReadAllFiles

--- src/DataSystem/AgentData.java
+++ src/DataSystem/AgentData.java
@@ -1,5 +1,6 @@
 package DataSystem;
 
+//todo: cari ways to get multiple data values with 1 accountID
 public class AgentData extends Data{
     AgentData(){};
 
--- src/FileSystem/FileConverter.java
+++ src/FileSystem/FileConverter.java
@@ -1,6 +1,7 @@
 package FileSystem;
 
 import DataSystem.AdminData;
+import DataSystem.AgentData;
 import DataSystem.Data;
 
 import java.io.File;
@@ -19,9 +20,10 @@ import java.util.Scanner;
 
 public class FileConverter {
     //pre converter
-    private static String[] lineSplitter(String lineToBeSplit) throws FileNotFoundException {
+    private static String[] lineSplitter(String lineToBeSplit){
         return lineToBeSplit.split("-");
     }
+
     private static String addDashIntoString(String[] stringToBeAdded) {
         StringBuilder content = new StringBuilder();
         for (String s : stringToBeAdded) {
@@ -49,6 +51,24 @@ public class FileConverter {
         return result;
     }
     //todo: make read the WHOLE FILE (especially utk location)
+    public static String[][] readAllLines(String filePath) throws IOException {
+        List<String> file = new ArrayList<>(
+                Files.readAllLines(Path.of("src/FileSystem/" + filePath), StandardCharsets.UTF_8)
+        );
+        String[] row = new String[file.size()];
+        for (int i = 0; i < file.size(); i++) {
+            row[i]= file.get(i);
+        }
+        System.out.println(Arrays.toString(row));
+        String[][] lineSplit = new String[row.length][lineSplitter(row[0]).length];
+        for (int i = 0; i < row.length; i++) {
+            String[] k = lineSplitter(row[i]);
+            for (int j = 0; j < k.length; j++) {
+                lineSplit[i][j] = k[j];
+            }
+        }
+        return lineSplit;
+    }
 
     //append file
     public static void appendFile(String filePath,String[] info) throws IOException {
@@ -58,6 +78,7 @@ public class FileConverter {
         Files.write(Paths.get("src/FileSystem/" + filePath), fileContent,
                 StandardOpenOption.APPEND);
     }
+    // update file
     private static void updateFile(String filePath, String[] oldInfo, String[] newInfo) throws IOException {
         String old = addDashIntoString(oldInfo);
         String newOne = addDashIntoString(newInfo);
@@ -78,9 +99,35 @@ public class FileConverter {
         data.setMainInfo(getSingleLineInfo("account.txt","TN2345"));
 
 
-        String[] oldInfo = data.getMainInfo();
-        data.setGender("female");
-        String[] newInfo = data.getMainInfo();
-        updateFile("testWrite.txt",oldInfo,newInfo);
+//        String[] oldInfo = data.getMainInfo();
+//        data.setGender("female");
+//        String[] newInfo = data.getMainInfo();
+//        updateFile("testWrite.txt",oldInfo,newInfo);
+        String[][] n = readAllLines("account.txt");
+        AdminData[] data1 = new AdminData[n.length];
+        for (int i = 0; i < 3; i++) {
+            data1[i] = new AdminData();
+            data1[i].setMainInfo(getSingleLineInfo("account.txt",n[i][0]));
+        }
+        System.out.println(Arrays.toString(data1[0].getMainInfo()));
+        System.out.println(Arrays.toString(data1[1].getMainInfo()));
+        System.out.println(Arrays.toString(data1[2].getMainInfo()));
+
+        data1[0].setPhoneNumber("test");
+        System.out.println(data1[0].getPhoneNumber());
+        System.out.println(data1[0].getAccountID());
+
+//        String[][] arr = new String[6][5];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i][0] = n[i];
+//        }
+//        System.out.println(arr[4][0]);
+//        System.out.println(Arrays.toString(lineSplitter(arr[4][0])));
+//        String[] p = lineSplitter(arr[4][0]);
+//        for (int i = 0; i < arr[4].length; i++) {
+//            arr[4][i] = p[i];
+//        }
+//        System.out.println(arr[4][1]);
+
     }
 }
