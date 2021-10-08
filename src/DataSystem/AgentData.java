3bd78effec07e818d174233b69457ffc3f81a9df
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-10-08 10:30:00 +0800

    update AgentData

--- src/DataSystem/AgentData.java
+++ src/DataSystem/AgentData.java
@@ -3,15 +3,5 @@ package DataSystem;
 //todo: cari ways to get multiple data values with 1 accountID
 public class AgentData extends Data{
     AgentData(){};
-
-    String houseType;
-    int price;
-    // location data
-    int houseNum;
-    String streetName;
-    String cityName;
-    int postcode;
-    String state;
-
-
+    private String locationID;
 }
